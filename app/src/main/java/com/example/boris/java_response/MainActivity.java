d19be0a09138abc04b66393c67160210e03535fb
Boris Kibirin <Boris Kibirin>
2016-06-15 20:36:15 +0300

    Initial commit

---
+++ app/src/main/java/com/example/boris/java_response/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.boris.java_response;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView myTextView = (TextView) findViewById(R.id.myText);
+        myTextView.setText("Android it's easy");
+
+        Button myBtn = (Button) findViewById(R.id.myBtn);
+        myBtn.setText("My first Button");
+        myBtn.setEnabled(false);
+
+        CheckBox myChb = (CheckBox) findViewById(R.id.myChb);
+        myChb.setChecked(true);
+    }
+}
